3e0d2da6961ba74a213e2c71c1d32006cbf0f357
mkdesign <me@mkdesign.ir>
2013-02-08 14:31:13 +0330

    site title finished

--- scripts.js
+++ scripts.js
@@ -5,11 +5,11 @@ $(function(){
 
 	slides = $('div.subdevision div.train > div');
 	slides.css({width:slidesWidth+'px'});
-	slides.last().css({'padding-right':'3500px'});
+	slides.last().css({'padding-right':'500px'});
 
 	slides.mouseover(function(){
-		slides.animate({width:slidesWidth-50+'px'},500);
-		$(this).animate({width:slidesWidth+250+'px'},500);
+		slides.animate({width:slidesWidth-20+'px'},500);
+		$(this).animate({width:slidesWidth+120+'px'},500);
 	});
 	$('div.subdevision').mouseout(function(){
 		slides.animate({width:slidesWidth+'px'},500);
